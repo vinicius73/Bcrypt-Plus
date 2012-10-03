f1ce592913e6132276fa63f97efe02274f613de4
Luiz Vinicius <luiz.vinicius73@gmail.com>
2012-10-03 11:53:02 -0300

    Gerador de Senha Adcicionado

--- Bcrypt.php
+++ Bcrypt.php
@@ -4,95 +4,117 @@
  * Bcrypt hashing class
  * 
  * @author Thiago Belem <contato@thiagobelem.net>
+ * @author Luiz Vinicius <contato@luizvinicius.com.br
  * @link   https://gist.github.com/3438461
  */
 class Bcrypt {
 
-/**
- * Default salt prefix
- * 
- * @see http://www.php.net/security/crypt_blowfish.php
- * 
- * @var string
- */
-	protected static $_saltPrefix = '2a';
+    /**
+     * Default salt prefix
+     * 
+     * @see http://www.php.net/security/crypt_blowfish.php
+     * 
+     * @var string
+     */
+    protected static $_saltPrefix = '2a';
 
-/**
- * Default hashing cost (4-31)
- * 
- * @var integer
- */
-	protected static $_defaultCost = 8;
+    /**
+     * Default hashing cost (4-31)
+     * 
+     * @var integer
+     */
+    protected static $_defaultCost = 8;
 
-/**
- * Salt limit length
- * 
- * @var integer
- */
-	protected static $_saltLength = 22;
+    /**
+     * Salt limit length
+     * 
+     * @var integer
+     */
+    protected static $_saltLength = 22;
 
-/**
- * Hash a string
- * 
- * @param  string  $string The string
- * @param  integer $cost   The hashing cost
- * 
- * @see    http://www.php.net/manual/en/function.crypt.php
- * 
- * @return string
- */
-	public static function hash($string, $cost = null) {
-		if (empty($cost)) {
-			$cost = self::$_defaultCost;
-		}
+    /**
+     * Lista de Caracteres
+     * 
+     * @var string
+     */
+    protected static $_ListaLetras = 'ABCDEFGHIJKLMNOPQRSTUVXYWZabcdefghijklmnopqrstuvxywz0123456789';
+    protected static $_ListaEspeciais = '*&@#+=$-%!.';
 
-		// Salt
-		$salt = self::generateRandomSalt();
+    /**
+     * Hash a string
+     * 
+     * @param  string  $string The string
+     * @param  integer $cost   The hashing cost
+     * 
+     * @see    http://www.php.net/manual/en/function.crypt.php
+     * 
+     * @return string
+     */
+    public static function hash($string, $cost = null) {
+        if (empty($cost)) {
+            $cost = self::$_defaultCost;
+        }
 
-		// Hash string
-		$hashString = self::__generateHashString((int)$cost, $salt);
+        // Salt
+        $salt = self::generateRandomSalt();
 
-		return crypt($string, $hashString);
-	}
+        // Hash string
+        $hashString = self::__generateHashString((int) $cost, $salt);
 
-/**
- * Check a hashed string
- * 
- * @param  string $string The string
- * @param  string $hash   The hash
- * 
- * @return boolean
- */
-	public static function check($string, $hash) {
-		return (crypt($string, $hash) === $hash);
-	}
+        return crypt($string, $hashString);
+    }
 
-/**
- * Generate a random base64 encoded salt
- * 
- * @return string
- */
-	public static function generateRandomSalt() {
-		// Salt seed
-		$seed = uniqid(mt_rand(), true);
+    /**
+     * Generate Random String
+     * @param integer $NumLetras Numero de letras na senha
+     * @param integer $NumEspeciais Numero de caracteres especiais na senha
+     */
+    public static function generateRandomString($NumLetras = 6, $NumEspeciais = 2) {
+        $Senha = str_shuffle(
+                substr(str_shuffle(self::$_ListaLetras), 0, $NumLetras) .
+                substr(str_shuffle(self::$_ListaEspeciais), 0, $NumEspeciais)
+        );
 
-		// Generate salt
-		$salt = base64_encode($seed);
-		$salt = str_replace('+', '.', $salt);
+        return $Senha;
+    }
 
-		return substr($salt, 0, self::$_saltLength);
-	}
+    /**
+     * Check a hashed string
+     * 
+     * @param  string $string The string
+     * @param  string $hash   The hash
+     * 
+     * @return boolean
+     */
+    public static function check($string, $hash) {
+        return (crypt($string, $hash) === $hash);
+    }
 
-/**
- * Build a hash string for crypt()
- * 
- * @param  integer $cost The hashing cost
- * @param  string $salt  The salt
- * 
- * @return string
- */
-	private static function __generateHashString($cost, $salt) {
-		return sprintf('$%s$%02d$%s$', self::$_saltPrefix, $cost, $salt);
-	}
+    /**
+     * Generate a random base64 encoded salt
+     * 
+     * @return string
+     */
+    public static function generateRandomSalt() {
+        // Salt seed
+        $seed = uniqid(mt_rand(), true);
 
+        // Generate salt
+        $salt = base64_encode($seed);
+        $salt = str_replace('+', '.', $salt);
+
+        return substr($salt, 0, self::$_saltLength);
+    }
+
+    /**
+     * Build a hash string for crypt()
+     * 
+     * @param  integer $cost The hashing cost
+     * @param  string $salt  The salt
+     * 
+     * @return string
+     */
+    private static function __generateHashString($cost, $salt) {
+        return sprintf('$%s$%02d$%s$', self::$_saltPrefix, $cost, $salt);
+    }
 }
